4da0dfdd9c5dce03041b95c9543f21793740613f
Zine Abidine <zine-abidine.oguad@polymtl.ca>
2021-09-18 02:05:09 +0800

    Exchange and place command validation

--- client/src/app/services/command-handler.service.ts
+++ client/src/app/services/command-handler.service.ts
@@ -7,6 +7,7 @@ export class CommandHandlerService {
     exchange(command: string): string {
         const regex = new RegExp(/^!exchange ([a-z]|\*){0,7}/g);
         if (regex.test(command)) {
+            // eslint-disable-next-line no-console
             console.log(command);
             return command;
         }
@@ -16,6 +17,7 @@ export class CommandHandlerService {
     place(command: string): string {
         const regex = new RegExp(/^!place ([a-o]([1-9]|1[0-5])(h|v)) ([a-zA-Z]){2,15}$/g);
         if (regex.test(command)) {
+            // eslint-disable-next-line no-console
             console.log(command);
             return command;
         }
