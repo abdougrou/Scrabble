f3c2f8f498794ee9ae5fffe4fe9d16ceffff1a8b
Abderrahim Laribi <alpyg@pm.me>
2021-10-09 20:06:14 -0400

    Fixed lint errors

--- server/app/classes/board.ts
+++ server/app/classes/board.ts
@@ -1,5 +1,5 @@
-import { BOARD_SIZE } from "@app/constants";
-import { Vec2 } from "./vec2";
+import { BOARD_SIZE } from '@app/constants';
+import { Vec2 } from './vec2';
 
 export class Board {
     data: string[][] = [];
@@ -9,7 +9,7 @@ export class Board {
      */
     initialize() {
         for (let x = 0; x < BOARD_SIZE; x++) {
-            let row: string[] = [];
+            const row: string[] = [];
             for (let y = 0; y < BOARD_SIZE; y++) {
                 row.push('');
             }
@@ -21,25 +21,27 @@ export class Board {
      * @returns a copy of the board
      */
     clone(): string[][] {
-        let newBoard: string[][] = [];
-        for (let i = 0; i < this.data.length; i++) {
-            newBoard.push(this.data[i].slice());
+        const newBoard: string[][] = [];
+        for (const row of this.data) {
+            newBoard.push(row.slice());
         }
         return newBoard;
     }
 
     /**
      * Places a letter in the board
+     *
      * @param coord tile coordinate to place the letter
      * @param letter the letter to place
      */
     setLetter(coord: Vec2, letter: string) {
+        this.data[coord.y][coord.x] = letter[0];
     }
 
     /**
      * @returns a transposed copy of the current board state
      */
     transpose(): string[][] {
-        return this.data[0].map((_, colIndex) => this.data.map(row => row[colIndex]));
+        return this.data[0].map((_, colIndex) => this.data.map((row) => row[colIndex]));
     }
 }
