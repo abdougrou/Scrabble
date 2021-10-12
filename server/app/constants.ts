0f3e7d1b254718a43e334e5a231cfa657ca0a405
Abderrahim Laribi <alpyg@pm.me>
2021-10-12 13:08:13 -0400

    Added Reserve class

--- server/app/constants.ts
+++ server/app/constants.ts
@@ -1,1 +1,29 @@
 export const BOARD_SIZE = 15;
+export const RESERVE_EXCHANGE_LIMIT = 7;
+export const CLASSIC_RESERVE = `A,9,1
+B,2,3
+C,2,3
+D,3,2
+E,15,1
+F,2,4
+G,2,2
+H,2,4
+I,8,1
+J,1,8
+K,1,10
+L,5,1
+M,3,2
+N,6,1
+O,6,1
+P,2,3
+Q,1,8
+R,6,1
+S,6,1
+T,6,1
+U,6,1
+V,2,4
+W,1,10
+X,1,10
+Y,1,10
+Z,1,10
+*,2,0`;
