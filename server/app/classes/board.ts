24542077501cf692e4da66e26d330d300592e4d8
Taha Ghodbane <abdelmouaiz-taha.ghodbane@polymtl.ca>
2021-11-05 20:06:45 -0400

    added board pointMultiplier

--- server/app/classes/board.ts
+++ server/app/classes/board.ts
@@ -1,25 +1,33 @@
-import { BOARD_SIZE } from '@app/constants';
+import { BOARD_SIZE, POINT_GRID } from '@app/constants';
 import { Vec2 } from './vec2';
 
+const BONUS_STR = '4444444433333333333333332222222222222111111111111111111111111';
 export class Board {
     /**
      * 2d character array storing letters
      */
     data: (string | null)[][] = [];
+    pointGrid: number[][] = [];
 
     /**
      * Initializes a 15x15 null matrix
      */
-    initialize() {
+    initialize(randomBonus: boolean) {
         for (let x = 0; x < BOARD_SIZE; x++) {
             const row: (string | null)[] = [];
             const anchorRow: boolean[] = [];
+            const pointRow: number[] = [];
             for (let y = 0; y < BOARD_SIZE; y++) {
                 row.push(null);
                 anchorRow.push(false);
+                pointRow.push(0);
             }
             this.data.push(row);
+            this.pointGrid.push(pointRow);
         }
+
+        if (!randomBonus) this.pointGrid = POINT_GRID;
+        else this.randomizeBonuses();
     }
 
     /**
@@ -51,5 +59,22 @@ export class Board {
      */
     getLetter(coord: Vec2): string | null {
         return this.data[coord.x][coord.y];
+    }
+
+    /**
+     * Replace bonuses with a random one
+     *
+     */
+    randomizeBonuses() {
+        const bonusStr = BONUS_STR;
+        for (let x = 0; x < BOARD_SIZE; x++) {
+            for (let y = 0; y < BOARD_SIZE; y++) {
+                const bonus = bonusStr.charAt(Math.floor(Math.random() * BONUS_STR.length));
+                if (POINT_GRID[x][y] !== 0 && bonus !== undefined) {
+                    this.pointGrid[x][y] = parseInt(bonus, 10);
+                    bonusStr.replace(bonus, '');
+                }
+            }
+        }
     }
 }
