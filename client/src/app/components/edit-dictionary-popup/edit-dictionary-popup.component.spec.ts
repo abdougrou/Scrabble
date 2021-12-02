cc166ec5937b5d5faf70dfe26ba1ec990719a466
khalilBedd <76928197+khalilBedd@users.noreply.github.com>
2021-12-01 23:02:56 -0500

    added more tests for dictionary components

--- client/src/app/components/edit-dictionary-popup/edit-dictionary-popup.component.spec.ts
+++ client/src/app/components/edit-dictionary-popup/edit-dictionary-popup.component.spec.ts
@@ -1,13 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DictionaryInfo } from '@common/dictionaryTemplate';
 import { EditDictionaryPopupComponent } from './edit-dictionary-popup.component';
+import SpyObj = jasmine.SpyObj;
 
 describe('EditDictionaryPopupComponent', () => {
     let component: EditDictionaryPopupComponent;
     let fixture: ComponentFixture<EditDictionaryPopupComponent>;
+    let dialogRefSpy: SpyObj<MatDialogRef<EditDictionaryPopupComponent>>;
+    const formConfig: DictionaryInfo = { title: 'testConfig', description: 'testDescription' };
+
+    beforeEach(() => {
+        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    });
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [EditDictionaryPopupComponent],
+            providers: [FormBuilder, { provide: MatDialogRef, useValue: dialogRefSpy }, { provide: MAT_DIALOG_DATA, useValue: formConfig }],
         }).compileComponents();
     });
 
@@ -19,5 +30,15 @@ describe('EditDictionaryPopupComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close the dialog when confirm is called', () => {
+        component.confirm();
+        expect(component.dialogRef.close).toHaveBeenCalled();
+    });
+
+    it('should close the dialog when cancel is called', () => {
+        component.cancel();
+        expect(component.dialogRef.close).toHaveBeenCalled();
     });
 });
