fb2f44e448ae0d402ad2397059bcd5747e71c99d
Taha Ghodbane <abdelmouaiz-taha.ghodbane@polymtl.ca>
2021-12-03 00:23:15 -0500

    testing dictionary popup. One function left to test

--- client/src/app/components/dictionary-popup/dictionary-popup.component.spec.ts
+++ client/src/app/components/dictionary-popup/dictionary-popup.component.spec.ts
@@ -1,15 +1,29 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 import { DictionaryPopupComponent } from './dictionary-popup.component';
 
 describe('DictionaryPopupComponent', () => {
     let component: DictionaryPopupComponent;
     let fixture: ComponentFixture<DictionaryPopupComponent>;
 
+    const dialogMock = {
+        close: () => {
+            // Do nothing
+        },
+        afterClosed: () => {
+            // Do nothing
+        },
+    };
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
+            imports: [HttpClientTestingModule, MatDialogModule, BrowserAnimationsModule],
             declarations: [DictionaryPopupComponent],
+            providers: [FormBuilder, { provide: MatDialogRef, useValue: dialogMock }],
         }).compileComponents();
     });
 
@@ -19,7 +33,59 @@ describe('DictionaryPopupComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('get dictionaries should call communication get dictionaries', () => {
+        const dictionary = [
+            {
+                title: 'Mon dictionaire',
+                description: 'Basic Dict',
+            },
+        ];
+        const spy = spyOn(component.communication, 'getDictionaryInfo').and.returnValue(of(dictionary));
+        component.getDictionaries();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('back should close', () => {
+        const spy = spyOn(component.dialogRef, 'close').and.callThrough();
+        component.back();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('edit should call modifyDictionary', () => {
+        const dictionary = {
+            title: 'Mon dictionaire',
+            description: 'Basic Dict',
+        };
+        const spy = spyOn(component.dialogRef, 'afterClosed').and.returnValue(of('sdfa'));
+        component.edit(dictionary);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('deleteDictionary should call communication deletedDictionary', () => {
+        const dictionary = {
+            title: 'Mon dictionaire',
+            description: 'Basic Dict',
+        };
+        const spy = spyOn(component.communication, 'deleteDictionary').and.callThrough();
+        component.deleteDictionary(dictionary);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('downloadDictionary should download it', () => {
+        const dictionary = {
+            title: 'Mon dictionaire',
+            description: 'Basic Dict',
+        };
+        const spy = spyOn(component.communication, 'getDictionaryFile').and.returnValue(of('sdf'));
+        component.downloadDictionary(dictionary);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('is default should return true if its the default dictionary', () => {
+        const dictionary = {
+            title: 'Mon dictionaire',
+            description: 'Basic Dict',
+        };
+        expect(component.isDefault(dictionary)).toBeTrue();
     });
 });
