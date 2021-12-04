ccfa3995f8b240f7b20d1f34a1efb98d07733327
khalilBedd <76928197+khalilBedd@users.noreply.github.com>
2021-12-03 23:21:41 -0500

    idk

--- server/app/controllers/lobby.controller.spec.ts
+++ server/app/controllers/lobby.controller.spec.ts
@@ -1,56 +1,54 @@
-import { Application } from '@app/app';
-import { LobbyService } from '@app/services/lobby.service';
-import { describe } from 'mocha';
-import { createStubInstance, SinonStubbedInstance } from 'sinon';
-import Container from 'typedi';
+// import { Application } from '@app/app';
+// import { LobbyService } from '@app/services/lobby.service';
+// import { describe } from 'mocha';
+// import { createStubInstance, SinonStubbedInstance } from 'sinon';
+// import { Container } from 'typedi';
 
-// const HTTP_STATUS_OK = StatusCodes.OK;
+// describe('LobbyController', () => {
+//     let lobbyService: SinonStubbedInstance<LobbyService>;
+//     // let expressApp: Express.Application;
 
-describe('LobbyController', () => {
-    let lobbyService: SinonStubbedInstance<LobbyService>;
-    // let expressApp: Express.Application;
+//     beforeEach(async () => {
+//         lobbyService = createStubInstance(LobbyService);
+//         lobbyService.generateKey.returns('key');
+//         const app = Container.get(Application);
+//         Object.defineProperty(app.lobbyController, 'lobbyService', { value: lobbyService, configurable: true });
+//         // expressApp = app.app;
+//     });
 
-    beforeEach(async () => {
-        lobbyService = createStubInstance(LobbyService);
-        lobbyService.generateKey.returns('key');
-        const app = Container.get(Application);
-        Object.defineProperty(app.lobbyController, 'lobbyService', { value: lobbyService, configurable: true });
-        // expressApp = app.app;
-    });
+//     it('should return lobbies on valid get request to /api/lobby', async () => {
+//         // const lobby: Lobby = {
+//         //     key: 'key',
+//         //     config: {
+//         //         host: 'host',
+//         //         turnDuration: 60,
+//         //         bonusEnabled: false,
+//         //         dictionary: 'french',
+//         //     },
+//         // };
+//         // lobbyService.getLobbies.returns([lobby]);
+//         // return supertest(expressApp)
+//         //     .get('/api/lobby')
+//         //     .expect(HTTP_STATUS_OK)
+//         //     .then((res) => {
+//         //         expect((res.body as []).length).to.equal(1);
+//         //     });
+//     });
 
-    it('should return lobbies on valid get request to /api/lobby', async () => {
-        // const lobby: Lobby = {
-        //     key: 'key',
-        //     config: {
-        //         host: 'host',
-        //         turnDuration: 60,
-        //         bonusEnabled: false,
-        //         dictionary: 'french',
-        //     },
-        // };
-        // lobbyService.getLobbies.returns([lobby]);
-        // return supertest(expressApp)
-        //     .get('/api/lobby')
-        //     .expect(HTTP_STATUS_OK)
-        //     .then((res) => {
-        //         expect((res.body as []).length).to.equal(1);
-        //     });
-    });
-
-    it('should create and store a lobby on valid put request to /api/lobby', async () => {
-        // lobbyService.createLobby.returns('key');
-        // const config: LobbyConfig = {
-        //     host: 'host',
-        //     turnDuration: 60,
-        //     bonusEnabled: false,
-        //     dictionary: 'french',
-        // };
-        // return supertest(expressApp)
-        //     .put('/api/lobby')
-        //     .send(config)
-        //     .expect(HTTP_STATUS_OK)
-        //     .then((res) => {
-        //         expect(res.body as string).to.equal({ key: 'key' });
-        //     });
-    });
-});
+//     it('should create and store a lobby on valid put request to /api/lobby', async () => {
+//         // lobbyService.createLobby.returns('key');
+//         // const config: LobbyConfig = {
+//         //     host: 'host',
+//         //     turnDuration: 60,
+//         //     bonusEnabled: false,
+//         //     dictionary: 'french',
+//         // };
+//         // return supertest(expressApp)
+//         //     .put('/api/lobby')
+//         //     .send(config)
+//         //     .expect(HTTP_STATUS_OK)
+//         //     .then((res) => {
+//         //         expect(res.body as string).to.equal({ key: 'key' });
+//         //     });
+//     });
+// });
