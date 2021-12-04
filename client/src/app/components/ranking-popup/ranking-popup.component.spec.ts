776c6e4b38ec268dd61e39ceb85f2a0f3d5bd0aa
khalilBedd <76928197+khalilBedd@users.noreply.github.com>
2021-12-03 20:03:08 -0500

    test

--- client/src/app/components/ranking-popup/ranking-popup.component.spec.ts
+++ client/src/app/components/ranking-popup/ranking-popup.component.spec.ts
@@ -2,16 +2,27 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppMaterialModule } from '@app/modules/material.module';
+import { CommunicationService } from '@app/services/communication.service';
+import { of } from 'rxjs';
 import { RankingPopupComponent } from './ranking-popup.component';
+import SpyObj = jasmine.SpyObj;
 
 describe('RankingPopupComponent', () => {
     let component: RankingPopupComponent;
+    let communicationSpy: SpyObj<CommunicationService>;
     let fixture: ComponentFixture<RankingPopupComponent>;
 
+    beforeEach(() => {
+        communicationSpy = jasmine.createSpyObj('CommunicationService', ['getClassicRanking', 'getLog2990Ranking']);
+    });
+
     beforeEach(async () => {
+        communicationSpy.getClassicRanking.and.returnValue(of([{ name: 'name', score: 2 }]));
+        communicationSpy.getLog2990Ranking.and.returnValue(of([{ name: 'name', score: 2 }]));
         await TestBed.configureTestingModule({
             imports: [HttpClientTestingModule, RouterTestingModule, AppMaterialModule],
             declarations: [RankingPopupComponent],
+            providers: [{ provide: CommunicationService, useValue: communicationSpy }],
         }).compileComponents();
     });
 
@@ -21,7 +32,7 @@ describe('RankingPopupComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
+    it('should create', async () => {
         expect(component).toBeTruthy();
     });
 });
